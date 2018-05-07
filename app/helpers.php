fa088b9a6f2a8cbb55ebe1052d805c542752df2e
Hung <hphamviet83@gmail.com>
2018-05-07 17:25:46 +0700

    them css vs js cua admin lte

---
+++ app/helpers.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+
+if (! function_exists('cdn_asset')) {
+    /**
+     * Generate a cdn asset path.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    function cdn_asset(string $path)
+    {
+        return \App\Utils::cdnAsset($path);
+    }
+}
+
+if (! function_exists('trustedproxy_config')) {
+    /**
+     * Get Trusted Proxy value
+     * @param string $key
+     * @param string $env_value
+     * 
+     * @return mixed
+     */
+    function trustedproxy_config($key, $env_value)
+    {
+        if ($key === 'proxies') {
+            if ($env_value === '*' || $env_value === '**') {
+                return $env_value;
+            }
+
+            return $env_value ? explode(',', $env_value) : null;
+        } 
+        elseif ($key === 'headers') {
+            if ($env_value === 'HEADER_X_FORWARDED_AWS_ELB') {
+                return Request::HEADER_X_FORWARDED_AWS_ELB;
+            } elseif($env_value === 'HEADER_FORWARDED') {
+                return Request::HEADER_FORWARDED;
+            }
+
+            return Request::HEADER_X_FORWARDED_ALL;
+        }
+
+        return null;
+    }
+}
